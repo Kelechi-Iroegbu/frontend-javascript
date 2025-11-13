1f266173828c8139a5ac908ffbdc43eea9c98c08
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2025-11-13 15:27:46 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,1 +1,12 @@
+// Subject.ts
+import { Subjects } from './Teacher';
 
+export namespace Subjects {
+  export class Subject {
+    teacher?: Teacher;
+
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
