5e3f1f42a51a0f373abcacda7fa5bedf8b6dd7b6
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2025-11-13 16:18:47 +0100

    Update Java.ts

--- 0x04-TypeScript/ task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/ task_4/js/subjects/Java.ts
@@ -1,22 +1,24 @@
-// Java.ts
-import { Subjects } from './Teacher';
-import './Subject';
+// task_4/js/subjects/Java.ts
+import { Subjects } from '../Teacher';
+import { Subject } from './Subject';
 
 export namespace Subjects {
+  // ✅ Using declaration merging to add a new optional attribute experienceTeachingJava? (number) to the Teacher interface
   export interface Teacher {
     experienceTeachingJava?: number;
   }
 
+  // class Java that extends Subject:
   export class Java extends Subject {
     getRequirements(): string {
       return 'Here is the list of requirements for Java';
     }
 
     getAvailableTeacher(): string {
-      if (this.teacher?.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
-        return `Available Teacher: ${this.teacher.firstName}`;
+      if (!this.teacher || this.teacher.experienceTeachingJava === undefined || this.teacher.experienceTeachingJava <= 0) {
+        return 'No available teacher';
       }
-      return 'No available teacher';
+      return `Available Teacher: ${this.teacher.firstName}`;
     }
   }
 }
